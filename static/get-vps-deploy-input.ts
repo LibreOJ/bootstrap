90d366ae5a2391f4658918ca29dd18d27bc25f86
Menci <huanghaorui301@gmail.com>
2022-08-17 01:29:08 +0800

    Run `yarn format`

--- static/get-vps-deploy-input.ts
+++ static/get-vps-deploy-input.ts
@@ -3,10 +3,12 @@ import "../config.ts";
 // @ts-ignore
 const config = (await import("../config.out.json")).default;
 
-console.log(JSON.stringify({
-  environment: process.argv[2],
-  data: {
-    "index.html": config.responseDataForRegion["CN"].body,
-    "sw.js": config.serviceWorker.body
-  }
-}));
+console.log(
+  JSON.stringify({
+    environment: process.argv[2],
+    data: {
+      "index.html": config.responseDataForRegion["CN"].body,
+      "sw.js": config.serviceWorker.body
+    }
+  })
+);
